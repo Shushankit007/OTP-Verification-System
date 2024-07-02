61e6ca7664f8e5032d930173631c777f7e567c2a
Shushankit007 <145269049+Shushankit007@users.noreply.github.com>
2024-07-02 20:32:23 +0530

    Update OTPverify_Backend.py

--- OTPverify_Backend.py
+++ OTPverify_Backend.py
@@ -74,6 +74,7 @@ class OTPVerifyFeatureBackend():
             print("OTP entered is invalid")
             return False
 
+# If you want to test this backend work without Interface, Uncomment below lines. 
     # Main function
     # def main(self):
     #     email = input("Enter your email address: ")
